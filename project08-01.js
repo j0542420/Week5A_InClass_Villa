5d32c6edad462d9d6ebfef158eb2ede77798cb3e
j0542420 <j0542420@amarillocollege.com>
2025-04-18 21:05:43 -0500

    Update project08-01.js

--- project08-01.js
+++ project08-01.js
@@ -11,24 +11,42 @@
 */
 
 /*--------------- Object Code --------------------*/
+// step 3
 function timer(min, sec){
-      timer.minutes = min
-      timer.seconds = sec
-      timer.timerID = null
+      this.minutes = min
+      this.seconds = sec
+      this.timerID = null
 }
-function runPause(timer,minBox,secBox){
-      if(timer.timedID == 0){
-            window.clearInterval(timer.timerID == null)
+// step 4 creating runPause method with 3 perameters timer,minBox,secBox
+timer.prototype.runPause = function(timer,minBox,secBox){
+      // step 5 using if else statement to pause the timer else the timer will start to countdown from 1000 milliseconds
+      if(timer.timeID){
+            window.clearInterval(timer.timeID);
+            timer.timeID = null;
       }
       else
       {
-            window.setInterval()
+            timer.timeID = window.setInterval(countdown, 1000);
+      }
+      // step 6 adding a countdown function 
+      function countdown(){
+            // 6a
+            if (timer.seconds > 0){
+                  timer.seconds -= 1;
+            // 6b
+            }else if (timer.minutes > 0){
+                  timer.minutes -= 1;
+                  timer.seconds = 59;
+            // 6c
+            }else {
+                  window.clearInterval(timer.timeID);
+                  timer.timeID = null;
+            }
+            // 6d
+            minBox.value = timer.minutes;
+            secBox.value = timer.seconds;
       }
 }
-
-
-
-
 
 
 /*---------------Interface Code -----------------*/
@@ -38,3 +56,19 @@ let minBox = document.getElementById("minutesBox");
 let secBox = document.getElementById("secondsBox");
 let runPauseTimer = document.getElementById("runPauseButton");
 
+// step 7 declaring myTimer with an instance of timer with the values of minBox.value and secBox.value
+let myTimer = new timer(parseInt(minBox.value), parseInt(secBox.value));
+
+// step 8 creating onchange event to minBox that sets myTimer.minutes to minBox.value
+minBox.onchange = function() {
+      myTimer.minutes = parseInt(minBox.value);
+};
+// creating onchange event to secBox that sets myTimer.seconds to secBox.value
+secBox.onchange = function() {
+      myTimer.seconds = parseInt(secBox.value);
+};
+
+// step 9 creating an onclick event for runPauseTimer()
+runPauseTimer.onclick = function(){
+      myTimer.runPause(myTimer,minBox,secBox)
+}
